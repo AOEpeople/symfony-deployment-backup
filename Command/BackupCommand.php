a86a96f2982ccf0503771fbc5e15f3f093a4f6f6
Steffen Ritter <info@rs-websystems.de>
2014-12-15 16:36:38 +0100

    [BUGFIX] Fix namespace

--- Command/BackupCommand.php
+++ Command/BackupCommand.php
@@ -1,5 +1,5 @@
 <?php
-namespace Acme\DemoBundle\Command;
+namespace Aoe\Deployment\SystemStorageBackupBundle\Command;
 
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputArgument;
