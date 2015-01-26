ca77ec78926dcf62899cbbb9b5e00c4d277fe3e0
Thomas Neumann <thomas.neumann@aoe.com>
2015-01-26 16:05:26 +0100

    [BUG] Fix erratic parameter in sprintf creating tar command

--- Command/RestoreCommand.php
+++ Command/RestoreCommand.php
@@ -110,7 +110,7 @@ class RestoreCommand extends ContainerAwareCommand {
         $backupFile = $backupDirectory . DIRECTORY_SEPARATOR . $input->getOption('restoreAssetsFilename');
         $this->checkBackupFile($backupFile);
 
-        $command = sprintf('tar -xzf %f', $backupFile);
+        $command = sprintf('tar -xzf %s', $backupFile);
         $process = new Process($command);
         $process->run();
 
